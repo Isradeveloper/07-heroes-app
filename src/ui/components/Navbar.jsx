a78a3cf30e883f9a392f5462edb5bc0855c588fb
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-19 11:48:10 -0500

    Navegación

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    navigate('/login', {
+      replace: true
+    })
+  }
+
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
 
@@ -37,7 +45,7 @@ export const Navbar = () => {
             Isra
           </span>
 
-          <button className='nav-item nav-link btn'>Logout</button>
+          <button className='nav-item nav-link btn' onClick={(e) => onLogout()}>Logout</button>
         </ul>
       </div>
     </nav>
