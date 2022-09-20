01614305e69e16d3725a423b270e73cd5bf81d0f
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-20 08:47:35 -0500

    Rutas publicas y privadas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,9 +7,10 @@ export const LoginPage = () => {
 
   const { login } = useContext(AuthContext)
   const onLogin = () => {
+    const lastPath = localStorage.getItem('lastPath') || '/'
     login('Israel')
 
-    navigate('/marvel', {
+    navigate(lastPath, {
       replace: true
     })
   }
